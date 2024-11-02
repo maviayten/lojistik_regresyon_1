2809be953f87077dd6e9e409cc0b012a5d567532
Ayten ERGİOĞUZ <141770507+maviayten@users.noreply.github.com>
2024-11-02 11:35:39 +0300

    Add files via upload

---
+++ lojistik_regresyon.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import pandas as pd
+water_quality_data = pd.read_csv(""C:\Users\User\OneDrive - MEF Üniversitesi\Masaüstü\lojistik_regresyon\waterQuality1.csv"")
+water_quality_data.head(20)
+
+
+# In[ ]:
+
+
+
+
